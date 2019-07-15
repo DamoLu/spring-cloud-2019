154a3282cad42873b39a4133ee94809759f3fceb
DamoLu <yanyanglu@foxmail.com>
2019-07-16 00:12:00 +0800

    first commit

---
+++ eureka-server/src/main/java/com/my/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Description: java类作用描述
+ * @Author: luqihua
+ * @CreateDate: 2019/7/15$ 23:45$
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
